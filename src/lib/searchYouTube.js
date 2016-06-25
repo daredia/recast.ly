1c402662627b97be7c1c800642f05ba25679f799
daredia <shehzad.daredia@gmail.com>
2016-06-25 10:56:39 -0700

    Make tests pass for searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,30 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // use jquery GET to search endpoint
+
+  var url = 'https://www.googleapis.com/youtube/v3/search';
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: {
+      type: 'video',
+      part: 'snippet',
+      q: options.query, 
+      maxResults: options.max, 
+      key: options.key,
+      format: 5
+    },
+    contentType: 'application/JSON',
+    success: (data) => {
+      console.log('successful GET. data.items:', data.items);
+      callback(data.items);
+    },
+    error: () => {
+      console.log('error on fetch');
+    }
+  }).done(() => {
+    console.log('done called');
+  });
 };
 
 window.searchYouTube = searchYouTube;
